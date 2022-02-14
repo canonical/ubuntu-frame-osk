507539b73f6ad452030138f13c942d9ff4c25bd4
William Wold <wm@wmww.sh>
2022-02-14 09:48:10 -0500

    Address reveiw

--- list-layouts/main.c
+++ list-layouts/main.c
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2022 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <assert.h>
 #define GNOME_DESKTOP_USE_UNSTABLE_API
@@ -66,7 +83,9 @@ void g_log_ignore (
     GLogLevelFlags log_level,
     const gchar* message,
     gpointer user_data
-) {}
+) {
+    (void)log_domain; (void)log_level; (void)message; (void)user_data;
+}
 
 
 int main() {
